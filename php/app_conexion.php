407ab60621c712ffc39deecf96e5b0039683882b
Fernando Mar <73912926+feralucard2@users.noreply.github.com>
2023-03-02 12:52:09 -0600

    conexion base de datos

--- php/app_conexion.php
+++ php/app_conexion.php
@@ -1,7 +1,7 @@
 <?php 
 	function conectarse() 
 	{	
-		$ser="localhost:3306"; 		$user="forge";		$pass="ipJlBz5ays4ho80oULXY";		$db="calculadoralg";
+		$ser="localhost:3306"; 		$user="forge";		$pass="3&-_spHXjB#Vk%FaU7jYBxUz";		$db="calculadoraprueba";
 				
 		$conexion = mysqli_connect($ser ,$user ,$pass) or die("Error de Conexión. Intente más tarde.");
 		$db = mysqli_select_db( $conexion, $db ) or die ( "Error. No hay conexión con la Base de Datos" );
